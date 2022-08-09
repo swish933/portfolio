cdf799bdcdcf047158a97f325a3130aa9dde6786
Ikem Nomso <ikemnomso@gmail.com>
2022-08-09 21:47:35 +0100

    seperated clone projects in ui

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import '../styles/Projects.css';
 import Works from './Project';
-import data from '../data/data';
+import {projects, clones} from '../data/data';
 import { Container } from 'react-bootstrap';
 
 const Projects = () => {
 	return (
 		<div className='Projects pt-5' id='work'>
 			<Container fluid='md' className='py-3'>
-				<p className='projects-header mt-5'>Selected Projects</p>
-				{data.map((project) => {
+				<p className='projects-header mt-3'>Selected Projects</p>
+				{projects.map((project) => {
+					return <Works project={project} />;
+				})}
+			</Container>
+			<Container fluid='md' className='py-3'>
+				<p className='projects-header mt-5 '>Cloned Pages</p>
+				{clones.map((project) => {
 					return <Works project={project} />;
 				})}
 			</Container>
---
+++ src/data/config.js
@@ -0,0 +1,3 @@
+export const resumeLink = 'https://drive.google.com/file/d/1JmSSaA9wYL3tTFw2kKCVe9eD1skOvPSc/view?usp=sharing';
+
+export const githubLink = 'https://github.com/swish933';

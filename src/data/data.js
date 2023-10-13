750e807d7519585e6d9aecfc5d16e2b27f7c48e1
Ikem Nomso <ikemnomso@gmail.com>
2023-10-13 22:50:34 +0100

    Updated site data.json

--- src/data/data.js
+++ src/data/data.js
@@ -1,47 +1,50 @@
 export const projects = [
-	{
-		name: 'Todo App',
-		description: 'A simple todo app ',
-		tools: 'React, Redux',
-		link: 'https://mptodo.netlify.app/',
-	},
-	{
-		name: 'IP Address Tracker',
-		description:
-			'A simple web app that shows the location of a domain or Ip address',
-		tools: 'React, LeafletJS Maps, Ipify API',
-		link: 'https://ipaddresstrackerr.netlify.app/',
-	},
-	{
-		name: 'Socio.io',
-		description: 'A simple mern social media app',
-		tools: 'React, Redux Toolkit, MongoDB, NodeJS & Express',
-		link: 'https://socio-io.herokuapp.com/',
-	},
+  {
+    name: "IP Address Tracker",
+    description:
+      "A simple web app that shows the location of a domain or Ip address",
+    tools: "React, LeafletJS Maps, Ipify API",
+    link: "https://ipaddresstrackerr.netlify.app/",
+  },
+  {
+    name: "Portfolio Site",
+    description: "A client portfolio site that showcases the clients work",
+    tools: "Next.js, TailwindCSS, Framer motion",
+    link: "https://georgeikem.com",
+  },
 
+  // {
+  // 	name: 'CMS app',
+  // 	description: 'Content management system backend',
+  // 	tools:'NodeJS, EJS views'
+  // 	link:'',
+  // }
 ];
 
-export const clones = [{
-	name: 'Blogr Landing Page',
-	description: 'A responsive landing page',
-	tools: 'HTML, SCSS, JS',
-	link: 'https://swish933.github.io/blogr-landing-page/',
-},
-{
-	name: 'Starbucks Page',
-	description: 'Unofficial starbucks rewards page',
-	tools: 'HTML, SCSS, JS',
-	link: 'https://swish933.github.io/starbucks-page-clone/',
-},
-{
-	name: 'Abstract Page',
-	description: 'A responsive page clone',
-	tools: 'HTML, CSS, JS',
-	link: 'https://swish933.github.io/abstract-page-clone/',
-},
-{
-	name: 'Manage Page',
-	description: 'A responsive page clone',
-	tools: 'HTML, TailwindCSS, JS',
-	link: 'https://swish933.github.io/tailwind-manage-landing/',
-}]
+export const clones = [
+  {
+    name: "Space Tourism Site",
+    description:
+      "A responsive website built with resources from frontendmentor.com",
+    tools: "HTML, CSS, JS",
+    link: "https://swish933.github.io/space-tourism-site/",
+  },
+  {
+    name: "Blogr Landing Page",
+    description: "A responsive landing page",
+    tools: "HTML, SCSS, JS",
+    link: "https://swish933.github.io/blogr-landing-page/",
+  },
+  {
+    name: "Starbucks Page",
+    description: "Unofficial starbucks rewards page",
+    tools: "HTML, SCSS, JS",
+    link: "https://swish933.github.io/starbucks-page-clone/",
+  },
+  {
+    name: "Manage Page",
+    description: "A responsive page clone",
+    tools: "HTML, TailwindCSS, JS",
+    link: "https://swish933.github.io/tailwind-manage-landing/",
+  },
+];
